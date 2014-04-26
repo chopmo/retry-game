5b7065633699f58a9d54aeb7e1c3037a33a08039
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2014-04-26 22:57:07 +0200

    Reformat

--- project.clj
+++ project.clj
@@ -1,12 +1,17 @@
 (defproject retry "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  
   :source-paths ["src/clj" "src/cljs"]
+  
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2069"]]
+  
   :plugins [[lein-cljsbuild "1.0.0"]]
+  
   :cljsbuild {:builds
               [{:source-paths ["src/cljs"]
                 :compiler {:output-to "resources/public/js/retry.js"
