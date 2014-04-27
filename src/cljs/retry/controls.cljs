18221452d932ed129f1612bc64c69512c61c2271
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2014-04-27 15:12:45 +0200

    Refactor

--- src/cljs/retry/controls.cljs
+++ src/cljs/retry/controls.cljs
@@ -1,14 +1,15 @@
 (ns retry.controls
   (:require [retry.js-utils :as utils]))
 
+(defn update-rotation [player keys]
+  (let [modifier (cond (:a keys) -0.1 (:d keys) 0.1 true 0)]
+    (assoc player :rotation (+ modifier (:rotation player)))))
+
+(defn update-acceleration [player keys]
+  (let [new-acceleration (if (:w keys) [0.3 0] [0 0])]
+    (assoc player :acceleration new-acceleration)))
+
 (defn update-player [player keys]
-  (let []
-    (assoc player
-      :rotation (+ (:rotation player)
-                   (cond
-                    (:a keys) -0.1
-                    (:d keys) 0.1
-                    true 0))
-      :acceleration (if (:w keys) [0.3 0] [0 0]))
-    
-    ))
+  (-> player
+      (update-rotation keys)
+      (update-acceleration keys)))
