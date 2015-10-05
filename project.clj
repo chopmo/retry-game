8aa57e73ef6966c88e6d9e48e179b939f84127e3
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2015-10-05 17:21:59 +0200

    Upgrade to compojure project

--- project.clj
+++ project.clj
@@ -1,19 +1,23 @@
 (defproject retry "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  
-  :source-paths ["src/clj" "src/cljs"]
-  
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2069"]]
-  
-  :plugins [[lein-cljsbuild "1.0.0"]]
-  
-  :cljsbuild {:builds
-              [{:source-paths ["src/cljs"]
-                :compiler {:output-to "resources/public/js/retry.js"
-                           :optimizations :whitespace
-                           :pretty-print true}}]})
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.3.1"]
+                 [ring/ring-defaults "0.1.2"]
+                 [org.clojure/clojurescript "0.0-2371"]
+                 ]
+  :plugins [[lein-ring "0.8.13"]
+            [lein-cljsbuild "1.0.3"]]
+  :ring {:handler retry.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}}
+  :cljsbuild {
+              :builds [{
+                        :source-paths ["src-cljs"]
+                        :compiler {
+                                   :output-to "resources/public/main.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]}
+  )
---
+++ src/retry/handler.clj
@@ -0,0 +1,12 @@
+(ns retry.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+(defroutes app-routes
+  (GET "/hello" [] "Hello World")
+  (GET "/robots.txt" [] "User-agent: *")
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
